93704e46ee4a750ced037b7fac6410d4bdc129b2
Pyunghyuk Yoo <yoophi@gmail.com>
2023-12-17 03:18:50 +0900

    feat: read file

--- ch12/minigrep/src/main.rs
+++ ch12/minigrep/src/main.rs
@@ -1,4 +1,4 @@
-use std::env;
+use std::{env, fs};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -7,4 +7,9 @@ fn main() {
 
     println!("검색어: {}", query);
     println!("대상 파일: {}", filename);
+
+    let contents = fs::read_to_string(filename)
+        .expect("파일을 읽지 못했습니다.");
+
+    println!("파일 내용:\n{}", contents);
 }
