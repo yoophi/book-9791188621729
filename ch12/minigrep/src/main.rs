e54650bc7f672797e0cd06097b83daa90c59cf81
Pyunghyuk Yoo <yoophi@gmail.com>
2023-12-17 03:21:22 +0900

    refactor: extrace parse_config

--- ch12/minigrep/src/main.rs
+++ ch12/minigrep/src/main.rs
@@ -2,8 +2,8 @@ use std::{env, fs};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let query = &args[1];
-    let filename = &args[2];
+
+    let (query, filename) = parse_config(&args);
 
     println!("검색어: {}", query);
     println!("대상 파일: {}", filename);
@@ -12,4 +12,11 @@ fn main() {
         .expect("파일을 읽지 못했습니다.");
 
     println!("파일 내용:\n{}", contents);
+}
+
+fn parse_config(p0: &Vec<String>) -> (&str, &str) {
+    let query = &args[1];
+    let filename = &args[2];
+
+    (query, filename)
 }
