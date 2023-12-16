1cd63429242a6087bf7c156eac9778d242bdc6bd
Pyunghyuk Yoo <yoophi@gmail.com>
2023-12-17 03:38:57 +0900

    refactor: 함수 및 struct 를 lib.rs 로 옮김

--- ch12/minigrep/src/main.rs
+++ ch12/minigrep/src/main.rs
@@ -1,6 +1,7 @@
-use std::{env, fs};
-use std::error::Error;
+use std::env;
 use std::process;
+
+use minigrep::{Config, run};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -16,32 +17,6 @@ fn main() {
     if let Err(e) = run(config) {
         println!("애플리케이션 에러: {}", e);
         process::exit(1);
-    }
-}
-
-fn run(config: Config) -> Result<(), Box<dyn Error>>{
-    let contents = fs::read_to_string(config.filename)?;
-
-    println!("파일 내용:\n{}", contents);
-
-    Ok(())
-}
-
-struct Config {
-    query: String,
-    filename: String,
-}
-
-impl Config {
-    fn new(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("필요한 인수가 지정되지 않았습니다.");
-        }
-
-        let query = args[1].clone();
-        let filename = args[2].clone();
-
-        Ok(Config { query, filename })
     }
 }
 
