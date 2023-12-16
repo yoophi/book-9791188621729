da1db3b97c6f11ac306b68820693ad46bbb8894a
Pyunghyuk Yoo <yoophi@gmail.com>
2023-12-17 03:15:39 +0900

    feat: set args

--- ch12/minigrep/src/main.rs
+++ ch12/minigrep/src/main.rs
@@ -1,6 +1,10 @@
 use std::env;
 
 fn main() {
-    let args : Vec<String> = env::args().collect();
-    println!("{:?}", args);
+    let args: Vec<String> = env::args().collect();
+    let query = &args[1];
+    let filename = &args[2];
+
+    println!("검색어: {}", query);
+    println!("대상 파일: {}", filename);
 }
